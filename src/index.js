22f28f898fabd6ea77fe71a9f03de9971a76f3ff
王国帅 <wangguoshuai@bytedance.com>
2019-07-02 08:07:34 +0800

    基本的服务实现

--- src/index.js
+++ src/index.js
@@ -1,1 +1,19 @@
-console.info('nams')
+const http = require('http')
+const chalk = require('chalk')
+const conf = require('./config/configDefault')
+
+const server = http.createServer((req, res) => {
+  res.statusCode = 200
+  res.setHeader('Content-Type', 'text/html')
+  res.write('<html>')
+  res.write('<body>')
+  res.write('Hello world!')
+  res.write('</body>')
+  res.write('</html>')
+  res.end()
+})
+
+server.listen(conf.port, conf.hostname, () => {
+  const addr = `http://${conf.hostname}:${conf.port}`
+  console.info(`Server start at ${chalk.green(addr)}`)
+})
