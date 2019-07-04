ac4b2b8687f56e676cfbd1b2f9f5f43a7ef13948
王国帅 <wangguoshuai@bytedance.com>
2019-07-04 08:08:37 +0800

    添加注释

--- src/helper/route.js
+++ src/helper/route.js
@@ -9,17 +9,17 @@ module.exports = async function(res, filePath) {
     if (stats.isFile()) {
       res.statusCode = 200
       res.setHeader('Content-Type', 'text/plain')
-      fs.createReadStream(filePath, 'utf-8').pipe(res)
+      fs.createReadStream(filePath, 'utf-8').pipe(res) // 1. 使用流的方式在高并发的情况下，表现更好
     } else if (stats.isDirectory()) {
       const files = await readdir(filePath)
       res.statusCode = 200
       res.setHeader('Content-Type', 'text/plain')
-      res.end(files.join(','))
+      res.end(files.join('\n'))
     }
   } catch (error) {
     res.statusCode = 404
     res.setHeader('Content-Type', 'text/plain')
-    res.end(`${filePath} is not file or directory`)
+    res.end(`${filePath} is not file or directory \n ${error}`)
     return
   }
 }
